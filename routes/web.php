d11c804e8bddf3312badfeae70917ba6fb739bb0
rahim <aboob3j@gmail.com>
2024-03-28 01:57:48 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,82 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use carbon\carbon ;
+use App\Http\Controllers\FisrtController;
+use App\Http\Controllers\nameController;
+use App\Http\Controllers\postscontroller;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\commentscontroller;
+use App\Mail\Discountoffer;
+use App\Models\comment;
+use App\Models\post;
+use App\Models\project;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Mail;
+
+use function Laravel\Prompts\table;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::resource('posts', postscontroller::class);
+Route::post('posts/{post}/comments', [commentscontroller::class, 'store']);
+
+Route::get('/message',function(){
+    App::setLocale('en');
+    echo(__('messages.welcome'));
+   
+});
+Route::get('/signup/{lang}',function($lang){
+    App::setlocale($lang);
+    return view('signup');
+});
+// Route::get('/mail',function(){
+//     Mail::raw('i love u',function($message){
+//         $message->to('abd@example.com')->subject('talk to me please');
+//     });
+// });
+Route::post('/mail',function(){
+   $email= request()->validate([
+        'email'=> 'required|email'
+    ]);
+    Mail::to($email)->send(new Discountoffer());
+    return back();
+});
+// // Route::post('/products',[ProductController::class,'createproduct']);
+
+
+// //or
+// Route::get('/posts/create',[postscontroller::class,'create']);
+
+
+
+// // Route::post('/posts',[postscontroller::class,'store']);
+
+
+// Route::get('/posts',[postscontroller::class,'index']);
+
+// Route::get('/posts/{id}',[postscontroller::class,'show']);
+// Route::post('/search_post',[postscontroller::class,'search']);
+// // Route::get('/a',[postscontroller::class,'a']);
+   
+
+
+
+
+// Route::get('/check',[ProjectController::class , 'check_user']);
+// Route::get('/get_names',[nameController::class , 'names']);
+
